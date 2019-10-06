3fd60ef1d0e5fdd01dfb14162f1ebd1d60219fa1
Katrina Fuller <trinafuller@att.net>
2019-10-05 21:54:46 -0600

    following along Pluralsight C# videos

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+
+      double result = 0.0;
+      foreach (double number in grades)
+      {
+        result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"The average grade is {result:N1}");  //the :N1 makes it so there is 1 number after the decimal
+
+      Console.WriteLine("Hello Katrina!");
     }
+  }
 }
